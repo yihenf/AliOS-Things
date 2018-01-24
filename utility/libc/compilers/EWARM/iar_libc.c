9697e5079b6352d4635a5b7db7029a57878edbc6
yihenf <yihenf@foxmail.com>
2018-01-24 11:36:31 +0800

    Revert "[LIBC IAR]Implement the _write_r function in libc_iar"
    This reverts commit e1f172ab002c1af3459edf431ef8548af9e44482.

--- utility/libc/compilers/EWARM/iar_libc.c
+++ utility/libc/compilers/EWARM/iar_libc.c
@@ -79,28 +79,9 @@ void __assert_func(const char * a, int b, const char * c, const char *d)
 }
 
 /*TO DO*/
-#pragma weak __write
-size_t __write(int handle, const unsigned char *buffer, size_t size)
+void __write()
 {
-    if (buffer == 0)
-    {
-        /*
-         * This means that we should flush internal buffers.  Since we don't we just return.
-         * (Remember, "handle" == -1 means that all handles should be flushed.)
-         */
-        return 0;
-    }
 
-    /* This function only writes to "standard out" and "standard err" for all other file handles it returns failure. */
-    if ((handle != 1) && (handle != 2))
-    {
-        return ((size_t)-1);
-    }
-
-    /* Send data. */
-    aos_uart_send(buffer, size, 1000);
-
-    return size;
 }
 
 void bzero()
